59acefc79e8b6ce1124653b9b37c114410d9d779
seimpairiyun <m.afizha96@gmail.com>
2022-06-04 12:24:32 +0700

    feat: adding sorter, sortAsc, sortDesc function

--- index.js
+++ index.js
@@ -6,15 +6,32 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, cb) => {
+  let n = [];
+
+  names.sort();
+
+  names.forEach((e, i) => {
+    n.push(`${i + 1}. ${e}`);
+  });
+
+  return cb(n);
+
+  //console.log(n);
+  //return n;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => {
+  return names.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names) => {
+  return names.reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
